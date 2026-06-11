82e0a4e546dce6d2ace470ea3bb2bd7a10f46f42
Verc <blizzadr@yandex.ru>
2026-06-11 11:03:17 +0300

    Rename lesson project to AVR Core

---
+++ 01_AVR_Fundamentals/Code/Lesson_01_GPIO/drivers/button.h
@@ -0,0 +1,18 @@
+/*
+ * button.h
+ *
+ * Created: 10.06.2026 17:08:32
+ *  Author: HP
+ */ 
+
+
+#ifndef BUTTON_H_
+#define BUTTON_H_
+
+#include <stdbool.h>
+
+void Button_Init(void);
+bool Button_IsPressed(void);
+bool Button_IsPressedDebounced(void);
+
+#endif /* BUTTON_H_ */
